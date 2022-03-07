b1185532fed8837e809f55202aa60a61be88b887
inoutshoppers <jspinn@csu.fullerton.edu>
2022-03-06 20:42:14 -0800

    Autocomplete store search

--- app/src/main/java/com/example/inoutshoppers/item/ItemList.kt
+++ app/src/main/java/com/example/inoutshoppers/item/ItemList.kt
@@ -9,6 +9,7 @@ import androidx.navigation.findNavController
 import com.example.inoutshoppers.R
 import com.example.inoutshoppers.databinding.ItemListBinding
 import com.example.inoutshoppers.databinding.LoginBinding
+import com.google.android.libraries.places.api.model.Place
 
 
 class ItemList : Fragment() {
@@ -19,6 +20,9 @@ class ItemList : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding = ItemListBinding.inflate(inflater, container, false)
+
+        val store : Place? = arguments?.getParcelable("selected_store")
+        binding.selectedStoreInfo.text = "You're shopping at: \n ${store?.name} \n ${store?.address}"
 
         binding.shopButton.setOnClickListener { view : View ->
             view.findNavController().navigate(ItemListDirections.actionItemListToShoppingNavigation())
