136336fcc2db2cea0ebbcd573b38b2dbf632fad4
inoutshoppers <jspinn@csu.fullerton.edu>
2022-04-20 19:45:53 -0700

    Sign out

--- app/src/main/java/com/example/inoutshoppers/home/Home.kt
+++ app/src/main/java/com/example/inoutshoppers/home/Home.kt
@@ -5,21 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import com.example.inoutshoppers.InOutShoppersApplication
 import com.example.inoutshoppers.R
+import com.example.inoutshoppers.authentication.AuthViewModelFactory
+import com.example.inoutshoppers.authentication.LoginViewModel
 import com.example.inoutshoppers.databinding.HomeBinding
 import com.example.inoutshoppers.databinding.SignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Home : Fragment() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var binding: HomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = HomeBinding.inflate(inflater, container, false)
+        binding = HomeBinding.inflate(inflater, container, false)
 
+        configureButtons();
+
+        return binding.root
+    }
+
+    private fun configureButtons() {
         binding.findStoreButton.setOnClickListener { view : View ->
             view.findNavController().navigate(HomeDirections.actionHomeToStoreSearch())
         }
@@ -28,7 +42,10 @@ class Home : Fragment() {
             view.findNavController().navigate(HomeDirections.actionHomeToAddItem())
         }
 
-        return binding.root
+        binding.signOutButton.setOnClickListener { view : View ->
+            firebaseAuth = (requireActivity().application as InOutShoppersApplication).firebaseAuth
+            firebaseAuth.signOut();
+            view.findNavController().navigate(HomeDirections.actionHomeToLogin())
+        }
     }
-
 }
